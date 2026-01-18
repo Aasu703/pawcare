009f11b51164b8c505209eb1b8cd2913311d6c10
Aayush Subedi <smellylips508@gmail.com>
2026-01-18 17:09:27 +0545

    cookies

--- lib/cookie.ts
+++ lib/cookie.ts
@@ -1,33 +1,43 @@
-"use server";
-import { cookies } from "next/headers";
+"use server"
+
+import { cookies } from "next/headers"
+
+interface UserData {
+    _id: string;
+    email: string;
+    username: string;
+    role: string;
+    createdAt: string;
+    updatedAt: string;
+    [key: string]: any;
+}
 
 export const setAuthToken = async (token: string) => {
     const cookieStore = await cookies();
-    cookieStore.set({ name: "auth_token", value: token })
+    cookieStore.set({
+        name: 'auth_token',
+        value: token,
+    })
 }
 export const getAuthToken = async () => {
     const cookieStore = await cookies();
-    const token = cookieStore.get("auth_token")?.value;
-    return token;
+    return cookieStore.get('auth_token')?.value || null;
 }
-export const setUserData = async (userData: any) => {
+export const setUserData = async (userData: UserData) => {
     const cookieStore = await cookies();
-    // cookie can only store string values
-    // convert object to string -> JSON.stringify
-    cookieStore.set({ name: "user_data", value: JSON.stringify(userData) })
+    cookieStore.set({
+        name: 'user_data',
+        value: JSON.stringify(userData),
+    })
 }
-export const getUserData = async () => {
+export const getUserData = async (): Promise<UserData | null> => {
     const cookieStore = await cookies();
-    const userDataStr = cookieStore.get("user_data")?.value;
-    // convert string back to object -> JSON.parse
-    if(userDataStr){
-        return JSON.parse(userDataStr);
-    }
-    return null;
+    const userData = cookieStore.get('user_data')?.value || null;
+    return userData ? JSON.parse(userData) : null;
 }
 
 export const clearAuthCookies = async () => {
     const cookieStore = await cookies();
-    cookieStore.delete("auth_token");
-    cookieStore.delete("user_data");
+    cookieStore.delete('auth_token');
+    cookieStore.delete('user_data');
 }
