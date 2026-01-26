f6b768be5bf17b234d3fd59a6d7b6ded8cd4d23d
Aayush Subedi <smellylips508@gmail.com>
2026-01-26 12:12:39 +0545

    profile page

--- proxy.ts
+++ proxy.ts
@@ -1,8 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getAuthToken, getUserData} from "./lib/cookie";
+import { is } from "zod/locales";
 
 const publicPaths = ["/login", "/register", "/forgot-password"];
 const adminPaths = ["/admin"];
+const userPaths = ["/user"];
 
 export async function proxy(req: NextRequest) {
 
@@ -14,7 +16,10 @@ export async function proxy(req: NextRequest) {
     const isPublicPath = publicPaths.some((path)=> pathname.startsWith(path));
 
     const isAdminPath = adminPaths.some((path)=> pathname.startsWith(path));
-
+    const isUserPath = userPaths.some((path)=> pathname.startsWith(path));
+    if(!user && !isPublicPath){
+        return NextResponse.redirect(new URL("/login", req.url));
+    }
 
     if(user && token){
         return NextResponse.redirect(new URL("/login", req.url));
@@ -25,6 +30,7 @@ export async function proxy(req: NextRequest) {
     if (isAdminPath && user.role !== 'admin') {
         return NextResponse.redirect(new URL("/login", req.url));
     }
+
 
 }
     
